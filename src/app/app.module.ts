1910c49d9810c06e75667528f5dae4b0b91ef91c
Wafa Waheeda Syed <wsyed@QCRI.org>
2021-07-04 05:20:11 +0300

    Register one sw

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,12 +13,6 @@ import { environment } from '../environments/environment';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ServiceWorkerModule.register('ngsw-worker.js', {
-      enabled: environment.production,
-      // Register the ServiceWorker as soon as the app is stable
-      // or after 30 seconds (whichever comes first).
-      registrationStrategy: 'registerWhenStable:30000'
-    }),
     ServiceWorkerModule.register('get-service-worker.js', { enabled: environment.production })
   ],
   providers: [],
--- src/get-service-worker.js
+++ src/get-service-worker.js
@@ -1,10 +1,11 @@
 importScripts('./ngsw-worker.js');
 
-window.addEventListener('DOMContentLoaded', () => {
-    const parsedUrl = new URL(window.location);
+  (function () {
+    self.addEventListener('DOMContentLoaded', (event) => {
+      const parsedUrl = new URL(window.location);
     // searchParams.get() will properly handle decoding the values.
     console.log('Title shared: ' + parsedUrl.searchParams.get('title'));
     console.log('Text shared: ' + parsedUrl.searchParams.get('text'));
     console.log('URL shared: ' + parsedUrl.searchParams.get('url'));
-  });
-
+    });
+  }());
